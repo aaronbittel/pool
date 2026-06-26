aaronbittel/pool

commit 37c355fa0589467e9a498e32e81a832e905215e0

feat(gossip): dont send messages others send us
broadcast node stores what others nodes have send, so when a gossip
event is triggered, those messages will not be send to these nodes

cmd/broadcast/main.go
@@ -60,15 +60,22 @@ func NewTopologyOkBody(messages []int) TopologyOkBody {
 	}
 }
 
+type set[T comparable] map[T]struct{}
+
 type BroadcastNode struct {
 	name      string
 	encoder   *json.Encoder
 	nextMsgID int
 
-	mutex    sync.Mutex // protects messages
-	messages map[int]struct{}
+	messageMutex sync.Mutex // protects messages
+	messages     set[int]
 
 	topology map[string][]string
+
+	knownMutex sync.Mutex // protects known
+	// all messages of a node that was sent to this node via a gossip message
+	// these messages dont not need to be sent to them in a gossip message
+	known map[string]set[int]
 }
 
 func (b *BroadcastNode) SetEncoder(encoder *json.Encoder) {
@@ -82,8 +89,8 @@ func (b *BroadcastNode) newID() int {
 }
 
 func (b *BroadcastNode) messageSlice() []int {
-	b.mutex.Lock()
-	defer b.mutex.Unlock()
+	b.messageMutex.Lock()
+	defer b.messageMutex.Unlock()
 	messages := make([]int, 0, len(b.messages))
 	for m := range b.messages {
 		messages = append(messages, m)
@@ -91,6 +98,7 @@ func (b *BroadcastNode) messageSlice() []int {
 	return messages
 }
 
+// maybe inject this via the node mainloop?
 func (b *BroadcastNode) gossip() {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
@@ -98,9 +106,18 @@ func (b *BroadcastNode) gossip() {
 		select {
 		case <-ticker.C:
 			for _, neighbor := range b.topology[b.name] {
+				sendMessages := []int{}
+				b.knownMutex.Lock()
+				for _, message := range b.messageSlice() {
+					if _, ok := b.known[neighbor][message]; !ok {
+						sendMessages = append(sendMessages, message)
+					}
+				}
+				b.knownMutex.Unlock()
+
 				body := GossipBody{
 					MsgBody:  node.MsgBody{Type: "gossip"},
-					Messages: b.messageSlice(),
+					Messages: sendMessages,
 				}
 				raw, err := json.Marshal(body)
 				if err != nil {
@@ -143,9 +160,9 @@ func (b *BroadcastNode) Step(msg node.Msg, encoder *json.Encoder) error {
 			return fmt.Errorf("could not unmarshal %v: %v", msg.RawBody, err)
 		}
 
-		b.mutex.Lock()
+		b.messageMutex.Lock()
 		b.messages[broadcastBody.Message] = struct{}{}
-		b.mutex.Unlock()
+		b.messageMutex.Unlock()
 
 		broadcastOkMsg, err := node.NewOkReply(msg, b.newID(), body.ID, "broadcast_ok")
 		if err != nil {
@@ -172,6 +189,11 @@ func (b *BroadcastNode) Step(msg node.Msg, encoder *json.Encoder) error {
 			return fmt.Errorf("could not unmarshal %v: %v", msg.RawBody, err)
 		}
 		b.topology = topologyBody.Topology
+		b.knownMutex.Lock()
+		for _, neigbour := range b.topology[b.name] {
+			b.known[neigbour] = make(set[int])
+		}
+		b.knownMutex.Unlock()
 		topologyOkMsg, err := node.NewOkReply(msg, b.newID(), body.ID, "topology_ok")
 		if err != nil {
 			return fmt.Errorf("could not create TopologyOk msg: %v", err)
@@ -185,11 +207,14 @@ func (b *BroadcastNode) Step(msg node.Msg, encoder *json.Encoder) error {
 		if err := json.Unmarshal(msg.RawBody, &gossipBody); err != nil {
 			panic(err)
 		}
-		b.mutex.Lock()
+		b.messageMutex.Lock()
+		b.knownMutex.Lock()
 		for _, m := range gossipBody.Messages {
+			b.known[msg.Src][m] = struct{}{}
 			b.messages[m] = struct{}{}
 		}
-		b.mutex.Unlock()
+		b.knownMutex.Unlock()
+		b.messageMutex.Unlock()
 	case "broadcast_ok", "read_ok", "topology_ok":
 	default:
 		panic(fmt.Sprintf("received unknown message type %q", body.Type))
@@ -199,5 +224,5 @@ func (b *BroadcastNode) Step(msg node.Msg, encoder *json.Encoder) error {
 }
 
 func main() {
-	node.MainLoop(&BroadcastNode{messages: make(map[int]struct{})})
+	node.MainLoop(&BroadcastNode{messages: make(set[int]), known: make(map[string]set[int])})
 }

commit 29144fa00ca3c1a937dbabd6fc4ca9fd11e08731

refactor: remove mutexes and switch to sequential event model
- a bug was introduced in commit 37c355f causing a deadlock, this was
  not caught by running the tests because the broadcast test was invoked
  with --node-count 1.

cmd/broadcast/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/aaronbittel/pool/internal/node"
@@ -72,16 +71,18 @@ type BroadcastNode struct {
 	name      string
 	nextMsgID int
 
-	messageMutex sync.Mutex // protects messages
-	messages     set[int]
+	messages set[int]
 
 	topology map[string][]string
 
-	knownMutex sync.Mutex // protects known
 	// all messages of a node that was sent to this node via a gossip message
 	// these messages dont not need to be sent to them in a gossip message
 	known map[string]set[int]
 }
+
+type GossipEvent struct{}
+
+func (GossipEvent) IsInjected() {}
 
 func (b *BroadcastNode) InitNode(events chan node.Event) {
 	b.nextMsgID = 0
@@ -93,7 +94,10 @@ func (b *BroadcastNode) InitNode(events chan node.Event) {
 		for {
 			select {
 			case <-ticker.C:
-				events <- node.Event{Kind: node.Injected}
+				events <- node.Event{
+					Kind:     node.Injected,
+					Injected: GossipEvent{},
+				}
 			}
 		}
 	}()
@@ -106,8 +110,6 @@ func (b *BroadcastNode) newID() int {
 }
 
 func (b *BroadcastNode) messageSlice() []int {
-	b.messageMutex.Lock()
-	defer b.messageMutex.Unlock()
 	messages := make([]int, 0, len(b.messages))
 	for m := range b.messages {
 		messages = append(messages, m)
@@ -121,16 +123,14 @@ func (b *BroadcastNode) Step(event node.Event, encoder *json.Encoder) error {
 		// received an event to do gossip
 		for _, neighbor := range b.topology[b.name] {
 			sendMessages := []int{}
-			b.knownMutex.Lock()
 			for _, message := range b.messageSlice() {
 				if _, ok := b.known[neighbor][message]; !ok {
 					sendMessages = append(sendMessages, message)
 				}
 			}
-			b.knownMutex.Unlock()
 
 			body := GossipBody{
-				MsgBody:  node.MsgBody{Type: "gossip"},
+				MsgBody:  node.MsgBody{Type: "gossip", ID: b.newID()},
 				Messages: sendMessages,
 			}
 			raw, err := json.Marshal(body)
@@ -171,9 +171,7 @@ func (b *BroadcastNode) Step(event node.Event, encoder *json.Encoder) error {
 				return fmt.Errorf("could not unmarshal %v: %v", msg.RawBody, err)
 			}
 
-			b.messageMutex.Lock()
 			b.messages[broadcastBody.Message] = struct{}{}
-			b.messageMutex.Unlock()
 
 			broadcastOkMsg, err := node.NewOkReply(msg, b.newID(), body.ID, "broadcast_ok")
 			if err != nil {
@@ -200,11 +198,9 @@ func (b *BroadcastNode) Step(event node.Event, encoder *json.Encoder) error {
 				return fmt.Errorf("could not unmarshal %v: %v", msg.RawBody, err)
 			}
 			b.topology = topologyBody.Topology
-			b.knownMutex.Lock()
 			for _, neigbour := range b.topology[b.name] {
 				b.known[neigbour] = make(set[int])
 			}
-			b.knownMutex.Unlock()
 			topologyOkMsg, err := node.NewOkReply(msg, b.newID(), body.ID, "topology_ok")
 			if err != nil {
 				return fmt.Errorf("could not create TopologyOk msg: %v", err)
@@ -217,14 +213,10 @@ func (b *BroadcastNode) Step(event node.Event, encoder *json.Encoder) error {
 			if err := json.Unmarshal(msg.RawBody, &gossipBody); err != nil {
 				panic(err)
 			}
-			b.messageMutex.Lock()
-			b.knownMutex.Lock()
 			for _, m := range gossipBody.Messages {
 				b.known[msg.Src][m] = struct{}{}
 				b.messages[m] = struct{}{}
 			}
-			b.knownMutex.Unlock()
-			b.messageMutex.Unlock()
 		case "broadcast_ok", "read_ok", "topology_ok":
 		default:
 			panic(fmt.Sprintf("received unknown message type %q", body.Type))

cmd/echo/main.go
@@ -43,13 +43,8 @@ func (e *EchoNode) newID() int {
 }
 
 func (e *EchoNode) Step(event node.Event, encoder *json.Encoder) error {
-	switch event.Kind {
-	case node.Injected:
+	if event.Kind != node.Message {
 		panic("got injected event when there's no event injection")
-	case node.Message:
-	// expected do nothing
-	default:
-		panic(fmt.Sprintf("got unexpected event kind %d", event.Kind))
 	}
 
 	msg := event.Msg

cmd/unique_ids/main.go
@@ -32,13 +32,8 @@ func (u *UniqueIdNode) newID() int {
 }
 
 func (u UniqueIdNode) Step(event node.Event, encoder *json.Encoder) error {
-	switch event.Kind {
-	case node.Injected:
+	if event.Kind != node.Message {
 		panic("got injected event when there's no event injection")
-	case node.Message:
-	// expected do nothing
-	default:
-		panic(fmt.Sprintf("got unexpected event kind %d", event.Kind))
 	}
 
 	msg := event.Msg

internal/node/node.go
@@ -14,9 +14,14 @@ const (
 	Injected
 )
 
+type InjectedMessage interface {
+	IsInjected()
+}
+
 type Event struct {
-	Kind MessageKind
-	Msg  Msg
+	Kind     MessageKind
+	Msg      *Msg
+	Injected InjectedMessage
 }
 
 type Msg struct {
@@ -25,7 +30,7 @@ type Msg struct {
 	RawBody json.RawMessage `json:"body"`
 }
 
-func NewReply(msg Msg, rawBody json.RawMessage) Msg {
+func NewReply(msg *Msg, rawBody json.RawMessage) Msg {
 	return Msg{
 		Src:     msg.Dst,
 		Dst:     msg.Src,
@@ -81,11 +86,11 @@ func readMessagesFromStdin(events chan Event) {
 			fmt.Fprintf(os.Stderr, "could not unmarshal stdin into msg: %v", err)
 			os.Exit(1)
 		}
-		events <- Event{Kind: Message, Msg: msg}
+		events <- Event{Kind: Message, Msg: &msg}
 	}
 }
 
-func ReplayToInit(msg Msg, msgID, replyMsgID int, encoder *json.Encoder) error {
+func ReplayToInit(msg *Msg, msgID, replyMsgID int, encoder *json.Encoder) error {
 	rawInitOK, err := json.Marshal(NewInitOKBody(msgID, replyMsgID))
 	if err != nil {
 		return fmt.Errorf("could not marshal initOkBody: %v", err)
@@ -106,7 +111,7 @@ func NewInitOKBody(msgID, replyMsgID int) InitOKBody {
 	}
 }
 
-func NewOkReply(msg Msg, msgID, replyMsgID int, typ string) (Msg, error) {
+func NewOkReply(msg *Msg, msgID, replyMsgID int, typ string) (Msg, error) {
 	body := MsgBody{
 		Type:      typ,
 		InReplyTo: replyMsgID,

tests/broadcast-partition-test.sh.todo
@@ -1,0 +1,3 @@
+#!/usr/bin/env bash
+
+./maelstrom/maelstrom test -w broadcast --bin ./bin/broadcast --node-count 5 --time-limit 20 --rate 10 --nemesis partition

tests/broadcast-test.sh
@@ -1,0 +1,3 @@
+#!/usr/bin/env bash
+
+./maelstrom/maelstrom test -w broadcast --bin ./bin/broadcast --node-count 5 --time-limit 20 --rate 10

tests/echo-test.sh
@@ -1,0 +1,3 @@
+#!/usr/bin/env bash
+
+./maelstrom/maelstrom test -w echo --bin ./bin/echo --time-limit 5

tests/run_all.go
@@ -1,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+const testDir = "./tests/"
+
+func main() {
+	fmt.Println("Build all executables...")
+	cmd := exec.Command("go", "build", "./...")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	tests, err := filepath.Glob(testDir + "*.sh")
+	if err != nil {
+		log.Fatalf("glob failed: %v", err)
+	}
+
+	results := map[string]bool{}
+
+	fmt.Println("Starting test runner...")
+	for _, path := range tests {
+		cmd := exec.Command(path)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			var exitErr *exec.ExitError
+			switch {
+			case errors.As(err, &exitErr):
+				results[path] = false
+			default:
+				log.Fatal("could not run cmd for %s %v: %v", path, cmd, err)
+			}
+		} else {
+			results[path] = true
+		}
+	}
+
+	fmt.Println("\nsummary:")
+	for _, path := range tests {
+		res := "SUCCESS"
+		if !results[path] {
+			res = "FAILED"
+		}
+		fmt.Printf("- %s: %s\n", path, res)
+	}
+}

tests/unique-ids-test.sh
@@ -1,0 +1,3 @@
+#!/usr/bin/env bash
+
+./maelstrom/maelstrom test -w unique-ids --bin ./bin/unique_ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition